76ecd5b1a2dc5a9177518a41a456913a0afe18e6
Revant Nandgaonkar <revant.one@gmail.com>
2021-05-18 19:22:16 +0530

    fix: remove destroy_frappe() to improve request time

--- fast_frappe/main.py
+++ fast_frappe/main.py
@@ -2,7 +2,7 @@ import frappe
 from typing import Optional
 
 from fastapi import FastAPI
-from fast_frappe.ctrl import init_frappe, destroy_frappe
+from fast_frappe.ctrl import init_frappe
 
 app = FastAPI()
 
@@ -12,7 +12,6 @@ def read_root():
     init_frappe()
     available_doctypes = frappe.get_list("DocType")
     settings = frappe.get_single("System Settings")
-    destroy_frappe()
     return {
         "available_doctypes": available_doctypes,
         "settings": settings.as_dict(),
